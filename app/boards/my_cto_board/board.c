f40fe5386485be7f542a373baa5143c1444562db
ctogit <torrescristian@outlook.com>
2026-05-06 03:31:57 +0000

    l5-task2: se crea una segunda carpeta dentro de boards, creando una sub-placa custom llamada my_cto_board con los archivos mínimos requeridos. Se observa compilación y flasheo correctos. Luego se crea archivo board.c y CMakeLists.txt para probar de ejecutar código antes que el src, se logra imprimir un msg en consola serie antes que el mensaje de la aplicación

---
+++ app/boards/my_cto_board/board.c
@@ -0,0 +1,26 @@
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
+#include <stdio.h>
+
+#define SLEEP_TIME_MS 2000
+
+static int board_my_cto_board_init(void) 
+{
+    /* Some function to run before boot */
+    k_msleep(SLEEP_TIME_MS);
+    printf("INITIALIZING MY_CTO_BOARD...\n");
+    k_msleep(SLEEP_TIME_MS);
+    printf("iomico zephyr course - lesson 5 (board definition)\n");
+    k_msleep(SLEEP_TIME_MS);
+
+    return 0;
+}
+/* 
+* Notas kernel level:
+* PRE_KERNEL_1: no arrancó la consola serie.
+* PRE_KERNEL_2: la consola muestra un msg de error de zephyr (MPU FAULT, ZEPHYR FATAL ERROR 20)
+* POST_KERNEL: funciona ok, se cuela un "Booting Zephyr OS build v4.4.0" al medio.
+* APPLICATION: funciona mejor (Booting Zephyr..., Initializaing..., HelloWorld)
+* SMP: igual que el anterior (aunque no se si se configuró bien) 
+*/
+SYS_INIT(board_my_cto_board_init, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Iomico l4-task1: crear una carpeta "boards" dentro de la raiz 
+ * Iomico l5-task1: crear una carpeta "boards" dentro de la raiz 
  * y copiar dentro de ella los archivos de la placa que se esté 
  * usando.Hay que editar los nombres de los archivos y contenido
  * según el nombre que le pongamos a la placa.
@@ -8,6 +8,17 @@
  * list(APPEND BOARD_ROOT ${CMAKE_CURRENT_SOURCE_DIR})
  * west build app -b cto_board_samv71/samv71q21 -p
  * En consola serie configurar según COM y 115200 baudios.
+ * 
+ * Iomico l5-task2: dentro de boards se creó otra placa con los
+ * archivos mínimos requeridos. En esta placa se pueden editar el 
+ * .dts para eliminar nodos que no se usan. 
+ * Para compilar:
+ * west build app -b my_cto_board/samv71q21
+ * 
+ * Dentro de la carpeta my_cto_board se creó archivo board.c y 
+ * CMakeLists.txt para probar de ejecutar código antes de que
+ * inicie el main y se jugó con diferentes niveles de kernel
+ * (ver board.c).
  */
 
 #include <zephyr/kernel.h>
